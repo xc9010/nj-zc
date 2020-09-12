0cbf9f3f23460a9a76f2e4670558857f6d853bcc
xc9010 <xuchen199010@gmail.com>
2020-09-12 11:48:36 +0800

    add

---
+++ src/components/Members.vue
@@ -0,0 +1,128 @@
+<template>
+<div class="member">
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="member-title">
+        <span>会员入会登记</span>
+      </div>
+      <div class="text item">
+        <el-form
+          ref="form"
+          :model="form"
+          label-width="180px"
+          size="medium"
+          inline
+        >
+          <el-form-item label="姓名">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="名族">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="出生年月">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="年龄">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="婚姻状况">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="学历">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="学位">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="政治面貌">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="身份证号码">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="联系电话">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="紧急联系方式">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="待入工会">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="申请入会时间">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="是否农民工">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="是否一线职工">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="工作单位">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="职务">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="户口所在地">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="现居住地">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="个人工作简历">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="简历附件">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="内容">
+            <el-input type="textarea" v-model="form.desc" maxlength="1000" rows="10"></el-input>
+          </el-form-item>
+        </el-form>
+        <div style="text-align: center">
+          <el-button type="primary">提交</el-button>
+          <el-button>重置</el-button>
+        </div>
+      </div>
+    </el-card>
+
+  </div>
+</div>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        levelList: null,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .member{
+    padding: 10px;
+    .member-title{
+      text-align: center;
+      font-size: 22px;
+      font-weight: bold;
+    }
+  }
+
+</style>
--- src/views/family/index.vue
+++ src/views/family/index.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="mainBox">
-    family
+    <div class="mainContent">
+      <el-tabs :tab-position="position">
+        <el-tab-pane label="我要入会">
+          <Members></Members>
+        </el-tab-pane>
+        <el-tab-pane label="职工创新">
+          <el-tabs type="border-card">
+            <el-tab-pane label="合理化建议">合理化建议</el-tab-pane>
+            <el-tab-pane label="先进操作法">先进操作法</el-tab-pane>
+            <el-tab-pane label="创新增效">创新增效</el-tab-pane>
+          </el-tabs>
+        </el-tab-pane>
+        <el-tab-pane label="红娘牵线"></el-tab-pane>
+        <el-tab-pane label="普惠服务"></el-tab-pane>
+        <el-tab-pane label="工会风采"></el-tab-pane>
+      </el-tabs>
+    </div>
   </div>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
+  import Card from '../../components/Card'
+  import Members from '../../components/Members'
 
   export default {
     name: 'Family',
+    components: { Card, Members },
+    data() {
+      return {
+        position: 'left',
+        active: ['1','2','3','4']
+      }
+    },
     computed: {
       ...mapGetters([
         'name'
       ])
+    },
+    methods: {
+      goto(url) {
+        console.log(url)
+        this.$router.push(url || './')
+      }
     }
   }
 </script>
